82235a4ad3e6138f44579558cccf3f1c2bce9cb2
Kwades <bz.jasiek@gmail.com>
2021-04-28 10:52:09 +0200

    Add choo choo time with a simple config

---
+++ choo-choo-time/main/config.h
@@ -0,0 +1,6 @@
+#ifndef CHOO_CHOO_TIME_H
+#define CHOO_CHOO_TIME_H
+
+unsigned int delayTime = 1000;
+
+#endif
---
+++ choo-choo-time/main/main.ino
@@ -0,0 +1,14 @@
+#include "config.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("I'm alive!");
+  delay(1000);
+
+}
